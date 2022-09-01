999be7edfbae3197b0212210d64ce1599c9c1ac4
scorpiofishingicecoffee <105265074+scorpiofishingicecoffee@users.noreply.github.com>
2022-09-01 13:30:40 +1000

    Init Commit

--- src/app.js
+++ src/app.js
@@ -10,24 +10,28 @@ const search = document.getElementById("searchUser");
 const button = document.getElementById("submit");
 button.addEventListener("click", (e) => {
   if (searchUser.value === "" || searchUser.value == null) {
-    alert("My dude! It's empty! Please enter an existing location!");
-  }
-
-  const currentVal = search.value;
-  ft.getCurrent(currentVal)
-    .then((data) => {
-      //call a UI method//
-      ui.populateUI(data);
-      //call saveToLS
-      ui.saveToLS(data);
-    })
-    .catch((e) => {
-      let text = `
-      <div class="card mx-auto mt-5" style="width: 50rem;">
+    let text = `
+              <div class="card mx-auto mt-5" style="width: 50rem;">
               <div class="card-body justify-content-center">
-<h3>Location doesn't exist! Please try again and check if the location does exist! <a href=https://geographyfieldwork.com/WorldCapitalCities.htm target=_blank>Please check the countries and locations here.</a></h3>`;
-      document.getElementById("content").innerHTML = text;
-    });
+              <h3>Please write something inside of the input field! <a href=https://geographyfieldwork.com/WorldCapitalCities.htm target=_blank>Please check the countries and locations here.</a></h3>`;
+    document.getElementById("content").innerHTML = text;
+  } else {
+    const currentVal = search.value;
+    ft.getCurrent(currentVal)
+      .then((data) => {
+        //call a UI method//
+        ui.populateUI(data);
+        //call saveToLS
+        ui.saveToLS(data);
+      })
+      .catch((e) => {
+        let text = `
+              <div class="card mx-auto mt-5" style="width: 50rem;">
+              <div class="card-body justify-content-center">
+              <h3>Location doesn't exist! Please try again and check if the location does exist! <a href=https://geographyfieldwork.com/WorldCapitalCities.htm target=_blank>Please check the countries and locations here.</a></h3>`;
+        document.getElementById("content").innerHTML = text;
+      });
+  }
 });
 
 //don't use jquery. doesn't work properly
